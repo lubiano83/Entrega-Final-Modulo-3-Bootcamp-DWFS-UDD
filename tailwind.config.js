99bf349dec6aef8424bdf9f9db6e46db47d7c6a9
lubiano83 <josepablolubiano@lubiano83.local>
2025-01-26 20:48:24 -0300

    primera carga

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js}', // Ajusta esto dependiendo de la estructura de tu proyecto
+      './public/**/*.html',
+      './**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
