8db7e5937a8a0ea4daed3368aa2b5c18a5bab787
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2025-01-27 17:35:37 -0300

    agregando whatsapp

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
       './**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-      extend: {},
-    },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },    
     plugins: [],
 };
