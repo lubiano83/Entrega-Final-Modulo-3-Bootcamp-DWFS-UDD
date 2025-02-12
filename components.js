097286b9b9f1450f1278fe0ceb099ee9bc6510ba
lubiano83 <josepablolubiano@lubiano83.local>
2025-02-12 11:53:10 -0300

    ajustes varios

--- components.js
+++ components.js
@@ -122,11 +122,11 @@ content.contacto = `<div class="flex flex-col justify-center items-start">
                     </div>`;
 
 content.equipo = `<div class="flex flex-col">
-                        <strong>Nuestro Equipo:</strong>
-                        <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/Jose%CC%81%20Pablo%20Lubiano%202.pdf?alt=media&token=faec1926-76bb-4369-aff6-933b12d17711" target="_blank" class="hover:text-gray-700 cursor-pointer">
-                            José Pablo Lubiano - FS
-                        </a>
-                    </div>`;
+                    <strong>Nuestro Equipo:</strong>
+                    <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/Jose%CC%81%20Pablo%20Lubiano%202.pdf?alt=media&token=faec1926-76bb-4369-aff6-933b12d17711" target="_blank" class="hover:text-gray-700 cursor-pointer">
+                        José Pablo Lubiano - FS
+                    </a>
+                </div>`;
               
 // Emulate a mini template engine:
 document.addEventListener('DOMContentLoaded', () => {
