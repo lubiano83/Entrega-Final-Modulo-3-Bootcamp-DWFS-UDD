b13948a359c9c89e880b3124349794ff488db240
lubiano83 <josepablolubiano@lubiano83.local>
2025-02-19 09:08:54 -0300

    ajustando href de logo

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,7 @@
 export default function logo() {
     return (
         `
-            <a href="https://lubiano83.github.io/Entrega-Final-Modulo-3-Bootcamp-DWFS-UDD/">
+            <a href="https://enjoytheapps.cl">
                 <h1 class="text-white font-bold italic text-2xl flex justify-center items-center gap-1 cursor-pointer hover:text-gray-700">
                     <p>Enjoy</p>
                     <p>The</p>
