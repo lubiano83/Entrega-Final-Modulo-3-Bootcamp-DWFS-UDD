c017d8a650c35faab845c446373a0f601be33f9c
lubiano83 <josepablolubiano@lubiano83.local>
2025-02-23 11:31:11 -0300

    colocando <strong> al menu

--- components/menu.js
+++ components/menu.js
@@ -2,10 +2,10 @@ export default function menu() {
     return (
         `
             <div class="w-full flex justify-evenly items-center gap-2 flex-wrap bg-blue-300 text-white py-1 px-2 xl:flex-col xl:justify-evenly xl:items-center xl:w-56 xl:h-screen xl:bg-blue-500">
-                <a href="#inicio" class="hover:text-gray-700">Inicio</a>
-                <a href="#nosotros" class="hover:text-gray-700">Nosotros</a>
-                <a href="#productos" class="hover:text-gray-700">Productos</a>
-                <a href="#mvp" class="hover:text-gray-700">MvP</a>
+                <a href="#inicio" class="hover:text-gray-700"><strong>Inicio</strong></a>
+                <a href="#nosotros" class="hover:text-gray-700"><strong>Nosotros</strong></a>
+                <a href="#productos" class="hover:text-gray-700"><strong>Productos</strong></a>
+                <a href="#mvp" class="hover:text-gray-700"><strong>MvP</strong></a>
             </div>
         `
     )
