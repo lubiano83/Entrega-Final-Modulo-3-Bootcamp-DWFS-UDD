a0d4e46b16c878d647ea624349bf3a1b10fce5d3
lubiano83 <josepablolubiano@lubiano83.local>
2025-02-23 11:53:32 -0300

    ajustando padding vista xl

--- components/mvp.js
+++ components/mvp.js
@@ -1,7 +1,7 @@
 export default function mvp() {
     return (
         `
-            <section class="w-screen flex justify-center items-center gap-8 flex-wrap pb-8 px-8 min-h-screen scroll-mt-28 xl:scroll-mt-6 xl:pl-56" id="mvp">
+            <section class="w-screen flex justify-center items-center gap-8 flex-wrap pb-8 px-8 min-h-screen scroll-mt-28 xl:scroll-mt-6 xl:pl-64" id="mvp">
                 <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/image-v2%2Fmvp.webp?alt=media&token=7d517e3f-5458-42ce-b5c3-15ca6dd08d76" alt="MVP enjoy the app" class="max-w-xl w-3/4 min-w-72">
                 <article class="flex flex-col gap-4 sm:max-w-xl md:max-w-xl lg:max-w-sm xl:max-w-sm w-3/4 min-w-72">
                     <strong class="text-xl text-blue-500">Haz crecer tu negocio con nosotros:</strong>
