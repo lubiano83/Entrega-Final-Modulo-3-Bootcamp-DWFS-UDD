56c534fe3203e9dfc7a91314fef9f1bc4bb5f6fc
lubiano83 <josepablolubiano@lubiano83.local>
2025-02-24 12:40:33 -0300

    agregando cambio de color segun ancho de pagina

--- components/inicio.js
+++ components/inicio.js
@@ -2,7 +2,7 @@ export default function inicio() {
     return (
         `
             <section class="flex flex-col justify-center items-center gap-4 max-w-5xl w-3/4 min-w-72 min-h-screen scroll-mt-4 xl:scroll-mt-2 xl:pl-64" id="inicio">
-                <strong class="text-2xl text-blue-500">¡Bienvenidos a Enjoy The App!</strong>
+                <strong class="text-2xl text-orange-500 xl:text-blue-500">¡Bienvenidos a Enjoy The App!</strong>
                 <h3>En Enjoy The App nos especializamos en desarrollo de software a medida, creando soluciones innovadoras que resuelven problemas reales y simplifican los procesos de nuestros clientes. Nuestra misión es transformar ideas en aplicaciones eficientes y funcionales, potenciando negocios de todos los tamaños con tecnología de vanguardia.</h3>
             </section>
         `
--- components/menu.js
+++ components/menu.js
@@ -1,7 +1,7 @@
 export default function menu() {
     return (
         `
-            <div class="w-full flex justify-evenly items-center gap-2 flex-wrap bg-blue-300 text-white py-1 px-2 xl:flex-col xl:justify-evenly xl:items-center xl:w-56 xl:h-screen xl:bg-blue-500">
+            <div class="w-full flex justify-evenly items-center gap-2 flex-wrap bg-orange-300 text-white py-1 px-2 xl:flex-col xl:justify-evenly xl:items-center xl:w-56 xl:h-screen xl:bg-blue-500">
                 <a href="#inicio" class="hover:text-gray-700">Inicio</a>
                 <a href="#nosotros" class="hover:text-gray-700">Nosotros</a>
                 <a href="#productos" class="hover:text-gray-700">Productos</a>
